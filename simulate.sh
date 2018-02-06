57586ef4409c5f6df35e0cbdc34dfde0b08c5659
Abhijit Theophilus <abhijit.theo@gmail.com>
2018-02-07 01:41:55 +0530

    Changed the color of the Icy World.

--- simulate.sh
+++ simulate.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 # Path to models.
+BASE_PATH=$(pwd)
 MODEL_PATH=$(pwd)/models
 
-# Check if GAZEBO_MODEL_PATH already has MODEL_PATH.
-case "${GAZEBO_MODEL_PATH:=$MODEL_PATH}" in
-    *:$MODEL_PATH:*) ;;
-    *) export GAZEBO_MODEL_PATH="$MODEL_PATH:$GAZEBO_MODEL_PATH" ;;
+# Check if GAZEBO_RESOURCE_PATH already has BASE_PATH.
+case "${GAZEBO_RESOURCE_PATH:=$BASE_PATH}" in
+    *:$BASE_PATH:*)
+        ;;
+    *)
+        export GAZEBO_RESOURCE_PATH="$BASE_PATH:$GAZEBO_RESOURCE_PATH"
+        export GAZEBO_MODEL_PATH="$MODEL_PATH:$GAZEBO_MODEL_PATH"
+        ;;
 esac
 
 gazebo worlds/icy.world --verbose
